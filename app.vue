1e9a914a197a89102dbf16e8210177879702fb81
Kevin Mueller <kevin.mueller@tuta.com>
2024-09-26 07:31:23 +0200

    added CustomCard component

--- app.vue
+++ app.vue
@@ -4,14 +4,23 @@
       <div class="half-elipse"></div>
       <h1>The Magnificent Seven Companies</h1>
     </div>
+    <div class="context">
+      <CustomCard>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, accusamus aut dignissimos dicta nemo
+          maiores, magni eum qui nam cumque consequatur molestiae tenetur! Vel, voluptatibus tempore. Aliquid suscipit
+          dolorum voluptates!</p>
+      </CustomCard>
+    </div>
+
   </div>
 </template>
 
 <script>
-export default{
-  name:'App',
-  components:{
-
+import CustomCard from './components/CustomCard.vue';
+export default {
+  name: 'App',
+  components: {
+    CustomCard
   }
 }
 </script>
