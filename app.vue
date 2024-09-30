f7d90074e0f297e213c2a98ac2e4652e15b567bb
Kevin Müller <kevin.mueller@tuta.com>
2024-09-30 14:33:41 +0200

    added scrollable widget content

--- app.vue
+++ app.vue
@@ -5,8 +5,11 @@
       <h1>The Magnificent Seven Companies</h1>
     </div>
     <div class="context">
-      <div class="ctx-top">
-        <Widget v-for="companie in companies" v-bind:companieData="companie"></Widget>
+      <div class="ctx-top" ref="ctxTop">
+        <div class="cards" ref="cards" v-on:mousedown="grabbed($event)" v-on:mouseup="grabbable"
+             v-on:mousemove="moveWidgets($event)">
+          <Widget v-for="companie in companies" v-bind:companieData="companie"></Widget>
+        </div>
       </div>
       <div class="ctx-mid">
         <CustomCard>
@@ -30,9 +33,6 @@ import Widget from './components/Widget.vue'
 import { stockService } from './service/stockService';
 
 
-useHead({
-  title: "title"
-})
 
 interface Company {
   abbreviation: string;
@@ -49,21 +49,70 @@ export default {
   data() {
     return {
       companies: [] as Company[],
-      netIncome: [] as any[]
+      netIncome: [] as any[],
+      slider: document.querySelector('.ctx-top') as HTMLElement,
+      cards: document.querySelector('.cards') as HTMLElement,
+      xPos: 0 as number,
+      isClicked: false as boolean,
+      dragSpeed: 0.7 as number
     }
   },
-  methods:{
+  methods: {
+    grabbable() {
+      this.isClicked = false;
+      const ctxTop = this.$refs.ctxTop as HTMLElement;
+      ctxTop.style.cursor = 'grab';
+    },
+
+    grabbed(event: MouseEvent) {
+      this.isClicked = true;
+      const ctxTop = this.$refs.ctxTop as HTMLElement;
+      const cards = this.$refs.cards as HTMLElement;
+
+      const rect = ctxTop.getBoundingClientRect();
+      this.xPos = event.clientX - rect.left - cards.offsetLeft;
+      console.log(this.xPos);
+
+      ctxTop.style.cursor = 'grabbing';
+    },
+
+    moveWidgets(event: MouseEvent) {
+      if (!this.isClicked) return;
+      event.preventDefault();
+      const ctxTop = this.$refs.ctxTop as HTMLElement;
+      const cards = this.$refs.cards as HTMLElement;
+
+      if (!cards) {
+        console.error('Cards element not found');
+        return;
+      }
+
+      const rect = ctxTop.getBoundingClientRect();
+      const newLeft = event.clientX - rect.left - this.xPos;
+      console.log(newLeft);
+
+      if (newLeft < -480) {
+        cards.style.left = `-480px`;
+      }else if(newLeft > -10){
+        cards.style.left = `-10px`
+      } else{
+        cards.style.left = `${newLeft}px`;
+      }
+    },
+
 
   },
   async created() {
+    window.addEventListener("mouseup", () => {
+      this.isClicked = false;
+    });
     this.companies = stockService.companies as Company[];
     this.companies.forEach(async abbr => {
-      const data = await stockService.getRevenue(abbr.abbreviation,abbr.netIncomeRow);
+      const data = await stockService.getRevenue(abbr.abbreviation, abbr.netIncomeRow);
       this.netIncome.push(data)
     });
   }
 }
-
 
 </script>
 
